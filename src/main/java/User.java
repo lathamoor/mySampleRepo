2686d761f832cf0dbf549daa23c84f0bf08eb024
¨lathamoor¨ <lathamoor@gmail.com>
2015-07-23 20:31:59 +0200

    My first commit for the CI Setup

---
+++ src/main/java/User.java
@@ -0,0 +1,34 @@
+/**
+ * Created by latha on 7/23/15.
+ */
+public class User {
+    private int userNo;
+    private int userNameIndex;
+    private String userName;
+    public int getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(int userNo) {
+        this.userNo = userNo;
+    }
+
+    public int getUserNameIndex() {
+        return userNameIndex;
+    }
+
+    public void setUserNameIndex(int userNameIndex) {
+        this.userNameIndex = userNameIndex;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
+}
